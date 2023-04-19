0a09e1b18ccac9fa39795a4907cd4629344bbfc0
tjd399 <thomas.j.davis@sherwin.com>
2023-04-19 13:43:20 -0400

    Create Azure Storage & Container

--- main.tf
+++ main.tf
@@ -1,13 +1,11 @@
 provider "azurerm" {
   features {}
+
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
 }
-
-
-
 
 # Check if the resource group already exists, else create it
 data "azurerm_resource_group" "example" {
