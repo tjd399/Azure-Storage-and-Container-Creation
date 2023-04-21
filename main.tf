a0511a981eb9f5b59277226ec3d722b9af62eb25
tjd399 <thomas.j.davis@sherwin.com>
2023-04-21 09:12:57 -0400

    Create Azure Storage & Container

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ data "azurerm_resource_group" "resource_group" {
  name = var.resource_group_name
 }
 
-/* resource "azurerm_resource_group" "resource_group" {
+resource "azurerm_resource_group" "resource_group" {
   name     = var.resource_group_name
   location = var.location
-} */
+} 
 
 
 # Create the storage account and container only if the resource group does not exist
