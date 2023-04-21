3e1fa1cf8a17c90ec5dc7f59c769e33d44ac1053
tjd399 <thomas.j.davis@sherwin.com>
2023-04-21 15:13:24 -0400

    Create Azure Storage & Container

--- main.tf
+++ main.tf
@@ -16,9 +16,9 @@ provider "azurerm" {
 
 
 # Check if the resource group already exists, else create it
-#data "azurerm_resource_group" "resource_group" {
- #name = var.resource_group_name
-#}
+data "azurerm_resource_group" "resource_group" {
+ name = var.resource_group_name
+}
 
 resource "azurerm_resource_group" "resource_group" {
   name     = var.resource_group_name
@@ -29,8 +29,6 @@ resource "azurerm_resource_group" "resource_group" {
  }
 }
 
-
-
 # Create the storage account and container only if the resource group does not exist
 resource "azurerm_storage_account" "storage" {
   name                     = var.storage_account_name
@@ -40,8 +38,8 @@ resource "azurerm_storage_account" "storage" {
   tags = {
     environment = "dev"
   }
-  resource_group_name       = azurerm_resource_group.resource_group.name
- # depends_on = [data.azurerm_resource_group.example]
+  resource_group_name       = data.azurerm_resource_group.resource_group.name
+  depends_on = [data.azurerm_resource_group.resource_group]
 }
 
 
@@ -50,6 +48,7 @@ resource "azurerm_storage_container" "storage_container" {
   name                  = var.container_name
   storage_account_name  = azurerm_storage_account.storage.name
   container_access_type = var.container_access_type
+  
 }
 
 
