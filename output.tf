a500d11d27cdfc45d6d1e2fc114530c78125373e
tjd399 <thomas.j.davis@sherwin.com>
2023-04-21 08:45:34 -0400

    Create Azure Storage & Container

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 // Define outputs
 output "storage_account_connection_string" {
   #count = length(var.resource_groups) 
-  value = azurerm_storage_account.example.primary_connection_string
+  value = azurerm_storage_account.storage.primary_connection_string
   sensitive = true
 }
 
